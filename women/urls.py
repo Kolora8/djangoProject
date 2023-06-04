6b94de2f560f1ce2de26b4ea335ef3ca3d1ce380
CracklyBody <m.ovchinnikov6@gmail.com>
2023-06-04 12:32:43 +0300

    Всё до 17 урока

--- women/urls.py
+++ women/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, re_path
-from django.utils import archive
 
 from .views import *
 
 urlpatterns = [
-    path('',index),
-    path('home/',index, name='home'),
-    path('about/', about, name='home')
-
+    path('', WomenHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
 ]
